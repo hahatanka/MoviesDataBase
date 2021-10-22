9aa46fa4eb88ed9377fa3f1838a643d306bb595e
Anna Kostenko <anna.double.y@gmail.com>
2021-10-22 21:31:33 +0300

    commit2

--- src/main/java/DBHandler.java
+++ src/main/java/DBHandler.java
@@ -2,6 +2,13 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DBHandler {
-    
+
+    public class DBHandler {
+
+        String connectionUrl = "jdbc:mysql://localhost:3306/MOVIE";
+        String user = "root";
+        String pass = "butjka12";
+
+        private static Connection connection;
+
 }
