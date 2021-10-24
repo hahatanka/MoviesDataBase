2b98c8da58fe97695af7178a47716b59a0b70767
Anna Kostenko <anna.double.y@gmail.com>
2021-10-24 15:55:41 +0300

    added update, delete, show all methods

--- src/main/java/DBHandler.java
+++ src/main/java/DBHandler.java
@@ -7,7 +7,7 @@ public class DBHandler {
     public static void main(String[] args) {
         String connectionUrl = "jdbc:mysql://localhost:3306/shop";
         String user = "root";
-        String pass = "butjka12";
+        String pass = "";
 
         Connection connection;
 
